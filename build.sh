01cff27a1f66c9d73e6095a800c51ab137fee6b7
HelioGuilherme66 <helioxentric@gmail.com>
2026-03-18 01:46:50 +0000

    Fix dependencies detected by Dependabot

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 
 # Tested on Fedora 42, Java 21, Python 13
 # export JAVA_HOME=/usr/lib/jvm/java
-export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-11.0.24.0.8-2.fc41.x86_64
-# export JAVA_HOME=/usr/lib/jvm/java-21-openjdk
-export JAVA_HOME=/usr/lib/jvm/java-24-openjdk
+# export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-11.0.24.0.8-2.fc41.x86_64
+export JAVA_HOME=/usr/lib/jvm/java-21-openjdk
+# export JAVA_HOME=/usr/lib/jvm/java-24-openjdk
 export DISABLE_SIKULI_LOG=1
 export PATH=$JAVA_HOME/bin:$PATH
 
